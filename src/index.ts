4162684cb51110b54f08db0aef2fd33c8be4ebbf
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-08-19 13:47:36 -0500

    add parseArray and parseNumber

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export function parseJSON(str: string) {
 
   function parseValue() {
     skipWhiteSpace();
-    const value = parseString() ?? parseObject();
+    const value = parseNumber() ?? parseString() ?? parseObject() ?? parseArray();
     skipWhiteSpace();
     return value;
   }
@@ -68,6 +68,70 @@ export function parseJSON(str: string) {
     }
   }
 
+  function parseArray(): any {
+    if (str[index] === "[") {
+      index++;
+      skipWhiteSpace();
+
+      const result = [];
+      let initial = true;
+
+      while (str[index] !== "]") {
+        if (!initial) {
+          eatComma();
+          skipWhiteSpace();
+        }
+        const value = parseValue();
+        result.push(value);
+        initial = false;
+      }
+      index++;
+      return result;
+    }
+  }
+
+  function parseNumber() {
+    let start = index;
+    skipWhiteSpace();
+    // Negative
+    if (str[index] === "-") {
+      index++;
+    }
+
+    // Integer part
+    if (str[index] === "0") {
+      index++;
+    } else if (str[index] >= "1" && str[index] <= "9") {
+      index++;
+      while (str[index] >= "0" && str[index] <= "9") {
+        index++;
+      }
+    }
+
+    // Floating part
+    if (str[index] === ".") {
+      index++;
+      while (str[index] >= "0" && str[index] <= "9") {
+        index++;
+      }
+    }
+
+    // Scientific notation
+    if (str[index] === "e" || str[index] === "E") {
+      index++;
+      if (str[index] === "-" || str[index] === "+") {
+        index++;
+      }
+      while (str[index] >= "0" && str[index] <= "9") {
+        index++;
+      }
+    }
+
+    if (index > start) {
+      return Number(str.slice(start, index));
+    }
+  }
+
   function skipWhiteSpace() {
     while (
       str[index] === " " ||
@@ -81,7 +145,7 @@ export function parseJSON(str: string) {
 
   function eatComma() {
     if (str[index] !== ",") {
-      throw new Error('Expected ",".');
+      throw new Error('Expected ","');
     }
     index++;
   }
@@ -96,5 +160,9 @@ export function parseJSON(str: string) {
 
 const result1 = parseJSON('"foobar"');
 const result2 = parseJSON('{ "foo": "bar" }');
+const result3 = parseJSON('["a", "b", "c"]');
+const result4 = parseJSON("[1, 2, 3, 4, 5]");
 console.log("parse strings: ", result1);
 console.log("parse objects: ", result2);
+console.log("parse arrays of strings: ", result3);
+console.log("parse arrays of numbers: ", result4);
